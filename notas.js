2adf5839a16f5c34cc63172ce35c630a46c0d7f3
Rene Amaral <renefamaral@gmail.com>
2021-05-25 10:19:06 -0300

    Primeiro commit

---
+++ notas.js
@@ -0,0 +1,32 @@
+/**
+ * Angular Material
+ * 
+ * O Angular Material é a implementação oficial para o Angular, do Material Design, a especificação de design para interfaces interartivas do Google. O Material Design cobre desde pequenos elementos, como ícones e cores, até elementos maiores como navegação, cards, imagem e muito mais.
+ * 
+ * https://material.angular.io/
+ * 
+ * Para utilizar o Angular Material é necessário instalar ele em nosso projeto
+ * 
+ * Dentro da pasta do projeto digitamos o comando: ng new mat   = o mat é o nome do nosso projeto
+ * 
+ * Receberemos a pergunta Would you like to add Angular routing = queremos adicionar rota Angular, respondemos yes
+ * 
+ * Após isso entramos na pasta criado, no caso a pasta mat e executamos o comando ng add @angular/material para adicionar ao nosso projeto
+ * 
+ * Recebemos a seguinte pergunta: Would you like to proceed?  = gostaria de continuar? Resposta Yes.
+ * 
+ * Choose a prebuilt theme name, or 'custom' for a custom theme = escolha um nome de tema pré-construído ou custom para um tema customizado. Somente apertar o enter para utilizar o tema Indigo/Pink. 
+ * 
+ * Após recebemos a seguinte pergunta: Set up global Angular Meterial typography styles? Configurar estilos de tipografia Angular Material globais? Resposta Yes
+ * 
+ * Após Set up browser animations for Angular Material? Resposta Yes.
+ * 
+ * 
+ * RxJS => é uma biblioteca para programação reativa usando Observables, para facilitar a composição de código assíncrono ou baseado em retorno de chamada.
+ * 
+ * https://www.learnrxjs.io/ => explica como funciona o RX, dá exemplos práticos, entre outras dicas.
+ * 
+ * 
+ * 
+ * 
+ */
